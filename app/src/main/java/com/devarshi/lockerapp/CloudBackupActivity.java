a010a1d163dd353934a79db4cfb5b8990dd1fd83
emergingcoders9 <emergingcoders9@gmail.com>
2022-03-11 18:49:57 +0530

    11/03/2022

--- app/src/main/java/com/devarshi/lockerapp/CloudBackupActivity.java
+++ app/src/main/java/com/devarshi/lockerapp/CloudBackupActivity.java
@@ -1,14 +1,23 @@
 package com.devarshi.lockerapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class CloudBackupActivity extends AppCompatActivity {
 
+
+    //Layouts, Views, etc.
+    ImageView imageViewExit;
+
+    //Intents and Variables
     Intent intent;
     ArrayList<String> itemsToBeBackedUpList;
 
@@ -17,7 +26,24 @@ public class CloudBackupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cloud_backup);
 
+        findViews();
+
+        initViews();
+
         intent = getIntent();
         itemsToBeBackedUpList = (ArrayList<String>) intent.getSerializableExtra("imagePaths");
+    }
+
+    private void findViews() {
+        imageViewExit = findViewById(R.id.exitImageView);
+    }
+
+    private void initViews() {
+        imageViewExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
